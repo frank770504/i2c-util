4a42947c5b4682b215b0f72ae570b9750ec31621
frank.yw.chen <frank.yw.chen@acer.com>
2014-12-23 11:42:29 +0800

    modify the printing method

--- main.c
+++ main.c
@@ -10,14 +10,29 @@
 #include <assert.h>
 #include <string.h>
 
-
 int main(int argc, char **argv)
 {
     bma2x2_t bma2x2;
     bma2x2acc_t acc;
     bma2x2_init(&bma2x2);
     printf("init ok: %x\n", bma2x2.chip_id);
-    bma2x2_read_accel_xyz(&acc);
-    printf("data|| x:%x y:%x z:%x\n", acc.x&0xFFF, acc.y&0xFFF, acc.z&0xFFF);
+    unsigned short ux, uy, uz;
+    short x, y, z;
+    while(1)
+    {
+        bma2x2_read_accel_xyz(&acc);
+        temp.x = acc.x;
+        temp.y = acc.y;
+        temp.z = acc.z;
+        ux = ((unsigned short)((temp.x&0x0FFF)<<4));
+        uy = ((unsigned short)((temp.y&0x0FFF)<<4));
+        uz = ((unsigned short)((temp.z&0x0FFF)<<4));
+        x = ((short)ux);
+        y = ((short)uy);
+        z = ((short)uz);
+        printf("%4x, %4x, %4x, %4d, %4d, %4d\n", ux, uy, uz, x, y, z);
+        usleep((useconds_t)(10000));
+        //i++;
+    }
     return 0;
 }
